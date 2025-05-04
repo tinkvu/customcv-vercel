20b0bca969ba58717e1f19347c9e4f4d34eb5f3b
Rinshad Choorappara <101931172+tinkvu@users.noreply.github.com>
2025-05-04 11:15:37 +0100

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from api.routes import upload, customize, generate_pdf
+from fastapi.staticfiles import StaticFiles
+app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
 
 app = FastAPI()
 
