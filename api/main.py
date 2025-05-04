6bc233e45421e5239c7bdcd6be37b2aca0d53929
Rinshad Choorappara <101931172+tinkvu@users.noreply.github.com>
2025-05-04 11:04:53 +0100

    Create main.py

---
+++ api/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from api.routes import upload, customize, generate_pdf
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(upload.router, prefix="/upload")
+app.include_router(customize.router, prefix="/customize")
+app.include_router(generate_pdf.router, prefix="/generate")
